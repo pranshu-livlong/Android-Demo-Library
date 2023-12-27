f83c31fe4dd7e708a3f35b08d6379aff377dc408
Pranshu <pranshu.kumar@livlong.com>
2023-12-27 11:47:33 +0530

    alert dialog added

--- app/src/main/java/com/example/mylibraryapplication/MainActivity.kt
+++ app/src/main/java/com/example/mylibraryapplication/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.mylibraryapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import com.example.mydemolibrary.Toaster
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toaster.simpleToast(applicationContext,"TOAST FROM THE LIBRARY")
-
+//        Toaster.simpleToast(applicationContext,"TOAST FROM THE LIBRARY")
+        val hello = findViewById<TextView>(R.id.textViewHello)
+        hello.setOnClickListener {
+            Toaster.showFullScreenDialog(this)
+        }
     }
 }
--- mydemolibrary/src/main/java/com/example/mydemolibrary/Toaster.kt
+++ mydemolibrary/src/main/java/com/example/mydemolibrary/Toaster.kt
@@ -1,6 +1,9 @@
 package com.example.mydemolibrary
 
+import android.app.AlertDialog
 import android.content.Context
+import android.view.LayoutInflater
+import android.widget.EditText
 import android.widget.Toast
 
 class Toaster {
@@ -8,6 +11,35 @@ class Toaster {
     companion object {
         fun simpleToast(context: Context, msg: String) {
             Toast.makeText(context, msg, Toast.LENGTH_LONG).show()
+        }
+
+        fun showFullScreenDialog(context: Context) {
+            val inflater = LayoutInflater.from(context)
+            val view = inflater.inflate(R.layout.full_screen_dialog_layout, null)
+
+            val dialog = AlertDialog.Builder(context)
+                .setView(view)
+                .setPositiveButton("OK") { dialog, _ ->
+                    // Handle the OK button click
+                    simpleToast(context, view.findViewById<EditText>(R.id.editText).text.toString())
+                    dialog.dismiss()
+                }
+                .setNegativeButton("Cancel") { dialog, _ ->
+                    // Handle the Cancel button click
+                    dialog.dismiss()
+                }
+                .create()
+
+            dialog.show()
+
+//            val window = dialog.window
+//            window?.setLayout(
+//                ViewGroup.LayoutParams.MATCH_PARENT,
+//                ViewGroup.LayoutParams.MATCH_PARENT
+//            )
+//            window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+//
+
         }
     }
 
